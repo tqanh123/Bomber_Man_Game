3b8e59a3449162b1ea64f1f7a95770228f59aa17
tqanhah <tqanh6203tkqn@gmail.com>
2024-01-04 22:01:21 +0700

    menu

--- src/game/constants/game.js
+++ src/game/constants/game.js
@@ -1,21 +1,10 @@
 /**
  * Set up you main game constant values here
  */
-// let n_player = 2;
-// let n_win = 3;
-let form = document.forms["my-form"];
-form.addEventListener("submit", getValues);
-function getValues(event) {
-  event.preventDefault();
+let n_player = 2;
+let n_win = 3;
 
-  // n_player = this.player.value;
-  // n_win = this.win.value;
-  //   export const NO_PLAYERS = 2;
-  //   export const MAX_WINS = 3;
-}
-
-
-export const FPS = 60;
+// export const FPS = 60;
 export const FRAME_TIME = 1000 / FPS;
 
 export const SCREEN_WIDTH = 256;
@@ -26,7 +15,6 @@ export const HALF_TILE_SIZE = TILE_SIZE / 2;
 
 export const STAGE_OFFSET_Y = 24;
 
-export const NO_PLAYERS = 2;
-export const MAX_WINS = 3;
-
+export let NO_PLAYERS = n_player;
+export let MAX_WINS = n_win;
 export const DEBUG = false;
